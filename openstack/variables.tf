cb90e09f3e428bff071df7c37f08271b7441e475
hoombah <hanwoolmeroomar@gmail.com>
2025-12-17 15:17:24 +0900

    Update variables.tf

--- openstack/variables.tf
+++ openstack/variables.tf
@@ -3,7 +3,7 @@
 variable "external_network_id" {
   description = "인터넷 연결을 위한 sharednet1의 UUID"
   type        = string
-  default     = 04a2babe-e5b2-4be7-b34a-eb5c4c750375 
+  default     = "04a2babe-e5b2-4be7-b34a-eb5c4c750375" 
 }
 
 variable "image_name" {
