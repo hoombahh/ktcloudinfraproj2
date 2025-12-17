c69e1a61321723583a987007be1c12f217015356
hoombah <hanwoolmeroomar@gmail.com>
2025-12-17 10:13:09 +0900

    Update backend.tf

--- openstack/backend.tf
+++ openstack/backend.tf
@@ -1,1 +1,11 @@
+# openstack/backend.tf
 
+terraform {
+  backend "s3" {
+    bucket         = "ktcloudinfraproj2-terraform-state" # 김기윤 팀원에게 확인받은 버킷명
+    key            = "active/terraform.tfstate"
+    region         = "ap-northeast-2"
+    encrypt        = true
+    # dynamodb_table = "terraform-lock" # 필요 시 설정
+  }
+}
