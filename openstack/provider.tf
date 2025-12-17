06bb3716ae0d3c2baa1af34efe3a2d0427b12a41
hoombah <hanwoolmeroomar@gmail.com>
2025-12-17 10:12:42 +0900

    Update provider.tf

--- openstack/provider.tf
+++ openstack/provider.tf
@@ -4,13 +4,12 @@ terraform {
   required_providers {
     openstack = {
       source  = "terraform-provider-openstack/openstack"
-      version = "~> 1.50.0" # 최신 안정 버전 사용
+      version = "~> 1.50.0"
     }
   }
 }
 
 provider "openstack" {
-  # 담당자에게 받은 clouds.yaml을 쓰거나 환경변수를 쓸 예정이므로 
-  # 여기엔 구체적인 ID/PW를 적지 않고 비워두는 것이 보안상 좋습니다.
+  # Jenkins 에이전트나 로컬 PC의 ~/.config/openstack/clouds.yaml 정보를 참조합니다.
   cloud = "openstack" 
 }
