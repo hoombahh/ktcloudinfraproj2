d46730dc52a9f5c6f46707cecf29949ceea48aa0
hoombah <hanwoolmeroomar@gmail.com>
2025-12-17 12:20:17 +0900

    Modify default values and descriptions in variables.tf
    Updated default values and descriptions for variables in variables.tf.

--- openstack/variables.tf
+++ openstack/variables.tf
@@ -3,23 +3,24 @@
 variable "external_network_id" {
   description = "인터넷 연결을 위한 sharednet1의 UUID"
   type        = string
-  default     = "여기에_sharednet1_ID_입력" 
+  default     = 04a2babe-e5b2-4be7-b34a-eb5c4c750375 
 }
 
 variable "image_name" {
   description = "인스턴스에 사용할 OS 이미지 이름"
   type        = string
-  default     = "Ubuntu-22.04" # 확인하신 이미지 중 하나 선택
+  default     = "Ubuntu" 
 }
 
 variable "flavor_name" {
-  description = "담당자가 추천하거나 이미 존재하는 플레이버 이름"
+  description = "인스턴스 사양 (Flavor)"
   type        = string
-  default     = "m1.medium"
+  # 확인하신 m1.medium을 정확히 입력합니다.
+  default     = "m1.medium" 
 }
 
 variable "key_pair" {
   description = "접속용 키페어 이름"
   type        = string
-  default     = "my-keypair"
+  default     = "active-keypair"
 }
