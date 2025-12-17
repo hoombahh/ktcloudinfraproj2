f614ee9c8f5c5780a1b23e7bcf419b1d11c734c6
hoombah <hanwoolmeroomar@gmail.com>
2025-12-17 10:15:16 +0900

    Refactor OpenStack main.tf with new network and rules
    Updated OpenStack configuration to reflect new naming conventions and added security group rules for HTTP and SSH.

--- openstack/main.tf
+++ openstack/main.tf
@@ -1,37 +1,36 @@
-# openstack/main.tf
-
-# 1. 전용 내부 네트워크 및 서브넷 생성
-resource "openstack_networking_network_v2" "active_net" {
-  name           = "active-internal-net"
+# 1. 네트워크 및 서브넷 생성 (담당자 가이드 반영)
+resource "openstack_networking_network_v2" "inner_net" {
+  name           = "kt-active-net"
   admin_state_up = "true"
 }
 
-resource "openstack_networking_subnet_v2" "active_subnet" {
-  name       = "active-internal-subnet"
-  network_id = openstack_networking_network_v2.active_net.id
-  cidr       = "192.168.10.0/24"
-  ip_version = 4
+resource "openstack_networking_subnet_v2" "inner_subnet" {
+  name            = "kt-active-subnet"
+  network_id      = openstack_networking_network_v2.inner_net.id
+  cidr            = "192.168.10.0/24" # 당신이 정한 내부 대역
+  ip_version      = 4
+  dns_nameservers = ["8.8.8.8", "8.8.4.4"]
 }
 
-# 2. 라우터 생성 및 외부망(sharednet1) 게이트웨이 설정
+# 2. 라우터 생성 및 외부망(sharednet1) 연결
 resource "openstack_networking_router_v2" "active_router" {
-  name                = "active-router"
+  name                = "kt-active-router"
   admin_state_up      = "true"
-  external_network_id = var.external_network_id
+  external_network_id = var.external_network_id # variables.tf에 sharednet1 ID 넣기
 }
 
-# 3. 라우터와 내부 서브넷 연결 (인터넷 통신 가능해짐)
-resource "openstack_networking_router_interface_v2" "router_intf" {
+resource "openstack_networking_router_interface_v2" "router_interface" {
   router_id = openstack_networking_router_v2.active_router.id
-  subnet_id = openstack_networking_subnet_v2.active_subnet.id
+  subnet_id = openstack_networking_subnet_v2.inner_subnet.id
 }
 
-# 4. 보안 그룹 및 규칙 생성 (직접 만드는 것이 관리상 유리)
+# 3. 보안 그룹 정의
 resource "openstack_networking_secgroup_v2" "active_sg" {
-  name        = "active-web-sg"
+  name        = "kt-active-sg"
   description = "Security group for Active Web Servers"
 }
 
+# HTTP(80) 허용 규칙
 resource "openstack_networking_secgroup_rule_v2" "rule_http" {
   direction         = "ingress"
   ethertype         = "IPv4"
@@ -42,16 +41,30 @@ resource "openstack_networking_secgroup_rule_v2" "rule_http" {
   security_group_id = openstack_networking_secgroup_v2.active_sg.id
 }
 
-# 5. 인스턴스 생성
-resource "openstack_compute_instance_v2" "web_server" {
+# SSH(22) 허용 규칙 (접속을 위해 추가 권장)
+resource "openstack_networking_secgroup_rule_v2" "rule_ssh" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 22
+  port_range_max    = 22
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.active_sg.id
+}
+
+# 4. 인스턴스 생성
+resource "openstack_compute_instance_v2" "active_web" {
   count           = 2
   name            = "active-web-${count.index + 1}"
-  image_name      = var.image_name
-  flavor_name     = var.flavor_name
+  image_name      = var.image_name 
+  flavor_name     = var.flavor_name # variables.tf에서 지정하거나 "m1.medium" 직접 기입
   key_pair        = var.key_pair
   security_groups = [openstack_networking_secgroup_v2.active_sg.name]
 
   network {
-    uuid = openstack_networking_network_v2.active_net.id
+    uuid = openstack_networking_network_v2.inner_net.id
   }
+
+  # 인스턴스 생성 시 서브넷이 먼저 준비되어야 하므로 종속성 명시
+  depends_on = [openstack_networking_subnet_v2.inner_subnet]
 }
