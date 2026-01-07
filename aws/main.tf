5807cbd7433535045a3a49b521eee0fd6ad26ecb
Jenkins <jenkins@test.com>
2026-01-07 14:55:43 +0900

    Add full infrastructure code

--- aws/main.tf
+++ aws/main.tf
@@ -1,1 +1,89 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+# 1. 네트워크 (VPC) - 집터 닦기
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "curry-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-northeast-2a", "ap-northeast-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
+
+# 2. 보안 그룹 (Security Group) - 대문 단속
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-security-group"
+  description = "Allow all inbound traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # 테스트용이라 다 열어둠 (실무에선 제한 필수)
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# 3. 데이터베이스 (RDS MariaDB) - 창고 짓기
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  db_name              = "mariadb"
+  engine               = "mariadb"
+  engine_version       = "10.6"
+  instance_class       = "db.t3.micro"
+  username             = "root"
+  password             = "test1234" # 초기 비밀번호
+  parameter_group_name = "default.mariadb10.6"
+  skip_final_snapshot  = true
+  publicly_accessible  = true
+  
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  db_subnet_group_name   = module.vpc.database_subnet_group_name
+}
+
+# 4. 쿠버네티스 (EKS) - 공장 짓기
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
+  cluster_name    = "curry-cluster" # Jenkinsfile 이름과 일치해야 함!
+  cluster_version = "1.27"
+
+  cluster_endpoint_public_access = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    default = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+
+      instance_types = ["t3.medium"]
+    }
+  }
+}
--- aws/outputs.tf
+++ aws/outputs.tf
@@ -1,8 +1,9 @@
-# 형님의 RDS 리소스 이름이 "default"라고 가정했습니다.
-# 만약 main.tf에서 resource "aws_db_instance" "mydb" 이렇게 되어 있으면
-# 아래 "aws_db_instance.default.address"를 "aws_db_instance.mydb.address"로 바꿔야 합니다!
-
 output "rds_endpoint" {
-  description = "The endpoint of the RDS instance"
-  value       = aws_db_instance.default.address 
+  description = "RDS 접속 주소"
+  value       = aws_db_instance.default.address
+}
+
+output "cluster_endpoint" {
+  description = "EKS 클러스터 주소"
+  value       = module.eks.cluster_endpoint
 }
